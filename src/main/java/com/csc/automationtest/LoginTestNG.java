a013bbdb7b85bd9d3da6e0c6d936f3b5e9561d65
tdang36 <tdang36@CSCVIEAE528186.vdc.csc.com>
2016-08-10 16:34:53 +0700

    TestNG - Login

--- src/main/java/com/csc/automationtest/LoginTestNG.java
+++ src/main/java/com/csc/automationtest/LoginTestNG.java
@@ -4,10 +4,7 @@ package com.csc.automationtest;
 
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
@@ -20,7 +17,6 @@ public class LoginTestNG {
 
 	private WebDriver driver;
 	  private String baseUrl;
-	  private boolean acceptNextAlert = true;
 	  private StringBuffer verificationErrors = new StringBuffer();
 
 	  @BeforeClass(alwaysRun = true)
@@ -35,7 +31,7 @@ public class LoginTestNG {
 	    driver.get(baseUrl + "");
 	    Assert.assertEquals(driver.getTitle(), "Login");
 	    driver.findElement(By.id("j_username")).clear();
-	    driver.findElement(By.id("j_username")).sendKeys("admin");
+	    driver.findElement(By.id("j_username")).sendKeys("admin");	    
 	    driver.findElement(By.id("j_password")).clear();
 	    driver.findElement(By.id("j_password")).sendKeys("admin");
 	    driver.findElement(By.xpath("//button[@type='submit']")).click();
@@ -51,38 +47,4 @@ public class LoginTestNG {
 	      Assert.fail(verificationErrorString);
 	    }
 	  }
-
-	  private boolean isElementPresent(By by) {
-	    try {
-	      driver.findElement(by);
-	      return true;
-	    } catch (NoSuchElementException e) {
-	      return false;
-	    }
-	  }
-
-	  private boolean isAlertPresent() {
-	    try {
-	      driver.switchTo().alert();
-	      return true;
-	    } catch (NoAlertPresentException e) {
-	      return false;
-	    }
-	  }
-
-	  private String closeAlertAndGetItsText() {
-	    try {
-	      Alert alert = driver.switchTo().alert();
-	      String alertText = alert.getText();
-	      if (acceptNextAlert) {
-	        alert.accept();
-	      } else {
-	        alert.dismiss();
-	      }
-	      return alertText;
-	    } finally {
-	      acceptNextAlert = true;
-	    }
-	  }
-
 }
