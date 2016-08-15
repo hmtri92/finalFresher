c90e565d7e386fbedc7bbf5a9af00799b4e97fa9
lle22 <quynhususu@gmail.com>
2016-08-15 10:13:38 +0700

    aaa

---
+++ src/test/java/com/csc/controller/LoginControllerTest.java
@@ -0,0 +1,29 @@
+package com.csc.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoginControllerTest {
+
+	@Test
+	public void testDoLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGoHomeHttpServletRequestModel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGoHome() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoginFail() {
+		fail("Not yet implemented");
+	}
+
+}
